177f7140f88d5902b2716bc613c10f0acfb7fae9
Abhishek <abhishekc838@gmail.com>
2024-06-24 21:55:06 +0530

    complited project

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,12 +1,19 @@
-import Hero from './Hero'
-import CustomerReviews from './CustomerReviews'
-import SuperQuality from './SuperQuality'
-import Subscribe from './SuperQuality'
-import Services from './Services'
-import SpecialOffer from './Specialoffer'
-import PopularProducts from './PopularProducts'
-import Footer from './Footer'
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
 
 export {
-    Hero, CustomerReviews, SuperQuality, Subscribe, Services, SpecialOffer, PopularProducts, Footer
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
 }
